c96c83f8d761bf878ff78a31d516975cec50d41d
sharmal33 <sharmal@101digital.io>
2022-05-12 11:50:36 +0530

    Version Update

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,14 +1,23 @@
-import { colors, fonts } from '../../assets';
-import { LoaderIcon } from '../../assets/icons';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { colors, fonts } from "../../assets";
+import { LoaderIcon } from "../../assets/icons";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  ActivityIndicator
+} from "react-native";
 
 const Loader = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Hang on for a moment</Text>
-      <Text style={styles.subTitle}>Hang on for a moment</Text>
-      <LoaderIcon width={60} height={60} />
+      <Text style={styles.subTitle}>We’re logging you in.</Text>
+      <ActivityIndicator
+        size={"large"}
+        style={styles.loadingIndicatorStyle}
+        color={colors.primary}
+      />
     </SafeAreaView>
   );
 };
@@ -17,9 +26,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center"
   },
   title: {
     fontFamily: fonts.bold,
@@ -28,18 +37,21 @@ const styles = StyleSheet.create({
     color: colors.primary,
     paddingHorizontal: 24,
     marginBottom: 32,
-    textAlign: 'center',
+    textAlign: "center"
   },
   subTitle: {
     fontFamily: fonts.bold,
     fontSize: 16,
     lineHeight: 24,
-    color: '#7F7B82',
+    color: "#7F7B82",
     paddingHorizontal: 24,
     marginBottom: 32,
-    textAlign: 'center',
-    paddingBottom: 100,
+    textAlign: "center",
+    paddingBottom: 100
   },
+  loadingIndicatorStyle: {
+    marginTop: 80
+  }
 });
 
 export default Loader;
